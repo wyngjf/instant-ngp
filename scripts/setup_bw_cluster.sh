237a9778229e675c39976f04134d54e3dfc75c8f
jianfenggaobit <jianfenggaobit@gmail.com>
2022-12-06 15:09:11 +0100

    update scripts

--- scripts/setup_bw_cluster.sh
+++ scripts/setup_bw_cluster.sh
@@ -1,24 +1,29 @@
-export ngp_path=$(pwd)
+export ngp_path
+ngp_path=$(pwd)
 
 function init_ngp(){
 	module load devel/cuda/11.6
 	module load vis/ffmpeg/5.1
 	export GLEW_INCLUDE_DIRS="$ngp_path/dependencies/glew/glew-2.1.0/include"
-	export GLEW_LIBRARIES="$ngp_path/dependencies/glew/glew-2.1.0/lib"
+	export GLEW_LIBRARIES="$ngp_path/dependencies/glew/glew-2.1.0/build/lib"
 }
 
 
 function setup_glew() {
-  local prev_pwd=$(pwd)
+  local prev_pwd
+  prev_pwd=$(pwd)
 
   mkdir -p "$ngp_path/dependencies/glew" &>/dev/null || true
-  cd "$ngp_path/dependencies/glew"
+  cd "$ngp_path/dependencies/glew" || return
 
   if [[ ! -d usr ]]; then
     wget "https://nav.dl.sourceforge.net/project/glew/glew/2.1.0/glew-2.1.0.tgz"
     tar zxvf glew-2.1.0.tgz
-    rm zxvf glew-2.1.0.tgz
+    rm glew-2.1.0.tgz
+    cd glew-2.1.0/build || return
+    cmake ./cmake
+    make -j8
   fi
 
-  cd "$prev_pwd"
+  cd "$prev_pwd" || return
 }
