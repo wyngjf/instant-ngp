bf82085797865c8bd2570de50abecb2db0e43558
tb3304@kit.edu <tb3304@uc2n997.localdomain>
2022-12-06 18:00:33 +0100

    update scripts

--- scripts/setup_bw_cluster.sh
+++ scripts/setup_bw_cluster.sh
@@ -2,28 +2,57 @@ export ngp_path
 ngp_path=$(pwd)
 
 function init_ngp(){
-	module load devel/cuda/11.6
+	module load devel/cuda/11.8
 	module load vis/ffmpeg/5.1
-	export GLEW_INCLUDE_DIRS="$ngp_path/dependencies/glew/glew-2.1.0/include"
+	module load devel/cmake/3.23.3
+	export GLEW_INCLUDE_DIR="$ngp_path/dependencies/glew/glew-2.1.0/include"
 	export GLEW_LIBRARIES="$ngp_path/dependencies/glew/glew-2.1.0/build/lib"
 }
 
 
+function compile() {
+	cd $ngp_path || return
+	TCNN_CUDA_ARCHITECTURES=70 cmake . -B build -DNGP_BUILD_ON_CLUSTER=ON
+	cmake --build build --config RelWithDebInfo -j60
+}
+
+
 function setup_glew() {
-  local prev_pwd
-  prev_pwd=$(pwd)
+	local prev_pwd
+	prev_pwd=$(pwd)
 
-  mkdir -p "$ngp_path/dependencies/glew" &>/dev/null || true
-  cd "$ngp_path/dependencies/glew" || return
+	mkdir -p "$ngp_path/dependencies/glew" &>/dev/null || true
+	cd "$ngp_path/dependencies/glew" || return
 
-  if [[ ! -d usr ]]; then
-    wget "https://nav.dl.sourceforge.net/project/glew/glew/2.1.0/glew-2.1.0.tgz"
-    tar zxvf glew-2.1.0.tgz
-    rm glew-2.1.0.tgz
-    cd glew-2.1.0/build || return
-    cmake ./cmake
-    make -j8
-  fi
+	if [[ ! -d usr ]]; then
+		wget "https://nav.dl.sourceforge.net/project/glew/glew/2.1.0/glew-2.1.0.tgz"
+		tar zxvf glew-2.1.0.tgz
+		rm glew-2.1.0.tgz
+		cd glew-2.1.0/build || return
+		cmake ./cmake
+		make -j60
+	fi
 
-  cd "$prev_pwd" || return
+	cd "$prev_pwd" || return
+}
+
+function setup_cmake() {
+	local prev_pwd
+	prev_pwd=$(pwd)
+
+	mkdir -p "$ngp_path/dependencies/cmake" &>/dev/null || true
+	cd "$ngp_path/dependencies/cmake" || return
+
+	if [[ ! -d "$ngp_path/dependencies/cmake/cmake-3.21.0" ]]; then
+		wget "https://cmake.org/files/v3.21/cmake-3.21.0.tar.gz"
+		tar zxvf cmake-3.21.0.tar.gz
+		rm cmake-3.21.0.tar.gz
+	fi
+
+	cd cmake-3.21.0 || return
+	./bootstrap
+	make -j60
+
+	cd "$prev_pwd" || return
+
 }
